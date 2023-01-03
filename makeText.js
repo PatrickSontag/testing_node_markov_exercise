6ba8b50ddb90904cb853464df0045ef1747c638a
Patrick Sontag <pmsontag84@gmail.com>
2023-01-03 07:12:42 -0600

    create text function

--- makeText.js
+++ makeText.js
@@ -1,2 +1,20 @@
-/** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const markov = require('./markov');
 
+const createText = (text) => {
+    console.log(text)
+    const markovMachine = new markov(text)
+    console.log(markovMachine.makeText())
+}
+
+const makeText = (path) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if(err) {
+            console.log(`ERROR reading ${path}: \n`, err);
+            process.exit(1)
+        }
+        else {
+            createText(data);
+        }
+    })
+}
